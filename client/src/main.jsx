d6aa9a3d33f540f87297f573de3e5bb3191b1d73
Dnyaneshwari <dnyaneshwari.2406487206@vcet.edu.in>
2026-04-23 23:58:49 +0530

    Prepare for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import axios from 'axios'
+
+// Configure Axios for Netlify Environment
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
